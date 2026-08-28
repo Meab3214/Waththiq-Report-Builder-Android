cb8c0799681f0499104d85fca4f4b1b753598f0e
Vv <lool3214@outlook.com>
2026-08-28 23:09:22 +0300

    Add capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.waththiq.reports',
+  appName: 'Waththiq',
+  webDir: 'dist'
+};
+
+export default config;
