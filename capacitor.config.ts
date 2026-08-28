6fdcfae92fd07524a28faa54aee67d2d9fae9dd2
Vv <lool3214@outlook.com>
2026-08-29 02:14:00 +0300

    feat: apply WASM official branding metadata

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.waththiq.reports',
-  appName: 'Waththiq',
+  appName: 'وَسْم',
   webDir: 'dist'
 };
 
